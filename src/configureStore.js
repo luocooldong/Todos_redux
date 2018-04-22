f50a2263f0faca3a1c42e2db8f517fb2db41ef85
gengyudong <gengyd@ivymobi.com>
2018-04-22 16:46:41 +0800

    refactoring-entry-point

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,24 +1,17 @@
-
 import { createStore } from 'redux';
-import todoApp from './reducers';
-
-import { saveState, loadState } from './localStorage';
-
 import throttle from 'lodash/throttle';
-
+import todoApp from './reducers';
+import { loadState, saveState } from './localStorage';
 
 const configureStore = () => {
   const persistedState = loadState();
-
   const store = createStore(todoApp, persistedState);
 
-  store.subscribe(
-    throttle(() => {
-      saveState({
-        todos: store.getState().todos,
-      });
-    }, 1000)
-  );
+  store.subscribe(throttle(() => {
+    saveState({
+      todos: store.getState().todos,
+    });
+  }, 1000));
 
   return store;
 };
