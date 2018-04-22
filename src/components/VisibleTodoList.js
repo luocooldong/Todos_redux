328b0a8fec8cc0b224ca9aad5cfb7de85c56ecf3
gengyudong <gengyd@ivymobi.com>
2018-04-22 22:57:30 +0800

    good commit

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,37 +1,16 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { toggleTodo } from '../actions';
+import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'SHOW_ALL':
-      return todos;
-    case 'SHOW_COMPLETED':
-      return todos.filter(t => t.completed);
-    case 'SHOW_ACTIVE':
-      return todos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}.`);
-  }
-};
-
-const mapStateToProps = (state, params) => ({
-  todos: getVisibleTodos(
-    state.todos,
-    params.filter || 'all'
-  ),
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  onTodoClick(id) {
-    dispatch(toggleTodo(id));
-  },
+const mapStateToProps = (state, { params }) => ({
+  todos: getVisibleTodos(state, params.filter || 'all'),
 });
 
 const VisibleTodoList = withRouter(connect(
   mapStateToProps,
-  mapDispatchToProps
+  { onTodoClick: toggleTodo }
 )(TodoList));
 
 export default VisibleTodoList;
