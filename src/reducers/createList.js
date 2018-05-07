57c9a98939f59050c6ced6a07e135834cd015fb0
gengyudong <gengyd@ivymobi.com>
2018-05-07 23:53:59 +0800

    21-displaying-loading-indicators

--- src/reducers/createList.js
+++ src/reducers/createList.js
@@ -1,13 +1,30 @@
 import { combineReducers } from 'redux';
 
-const createList = (filter) => { // eslint-disable-line arrow-body-style
+const createList = (filter) => {
+  const handleToggle = (state, action) => {
+    const { result: toggledId, entities } = action.response;
+    const { completed } = entities.todos[toggledId];
+    const shouldRemove = (
+      (completed && filter === 'active') ||
+      (!completed && filter === 'completed')
+    );
+    return shouldRemove ?
+      state.filter(id => id !== toggledId) :
+      state;
+  };
+
   const ids = (state = [], action) => {
-    if (filter !== action.filter) {
-      return state;
-    }
     switch (action.type) {
-      case 'RECEIVE_TODOS':
-        return action.response.map(todo => todo.id);
+      case 'FETCH_TODOS_SUCCESS':
+        return filter === action.filter ?
+          action.response.result :
+          state;
+      case 'ADD_TODO_SUCCESS':
+        return filter !== 'completed' ?
+          [...state, action.response.result] :
+          state;
+      case 'TOGGLE_TODO_SUCCESS':
+        return handleToggle(state, action);
       default:
         return state;
     }
@@ -18,10 +35,26 @@ const createList = (filter) => { // eslint-disable-line arrow-body-style
       return state;
     }
     switch (action.type) {
-      case 'REQUEST_TODOS':
+      case 'FETCH_TODOS_REQUEST':
         return true;
-      case 'RECEIVE_TODOS':
+      case 'FETCH_TODOS_SUCCESS':
+      case 'FETCH_TODOS_FAILURE':
         return false;
+      default:
+        return state;
+    }
+  };
+
+  const errorMessage = (state = null, action) => {
+    if (filter !== action.filter) {
+      return state;
+    }
+    switch (action.type) {
+      case 'FETCH_TODOS_FAILURE':
+        return action.message;
+      case 'FETCH_TODOS_REQUEST':
+      case 'FETCH_TODOS_SUCCESS':
+        return null;
       default:
         return state;
     }
@@ -30,10 +63,12 @@ const createList = (filter) => { // eslint-disable-line arrow-body-style
   return combineReducers({
     ids,
     isFetching,
+    errorMessage,
   });
 };
 
 export default createList;
 
 export const getIds = (state) => state.ids;
-export const getIsFetching = state => state.isFetching;
+export const getIsFetching = (state) => state.isFetching;
+export const getErrorMessage = (state) => state.errorMessage;
